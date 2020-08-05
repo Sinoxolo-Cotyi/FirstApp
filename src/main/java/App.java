dc3ca8465692ee0e5b50a4362d38b08384ff12b0
Sinoxolo-Cotyi <sincoty@gmail.com>
2020-08-05 14:44:18 +0200

    Added an arrayList to the main method of app.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,22 +1,27 @@
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import static spark.Spark.*;
-import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) {
         staticFiles.location("/public");
-       ArrayList<String> usernames = new ArrayList<String>();
-       usernames.add("username");
+        List<String> namesList = Arrays.asList("username");
+
+        //Copy all items from list 1 to list 2
+        ArrayList<String> usernames = new ArrayList<String>(namesList);
+
+        System.out.println(usernames);
+
        if (usernames.contains("username")) {
            System.out.println(usernames);
        } else {
            usernames.add("username");
            System.out.println(usernames);
        }
+
         get("/hello", (req, res) -> {
 
             Map<String, Object> map = new HashMap<>();
